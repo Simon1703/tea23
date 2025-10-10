74703d070dcd1e85c38ffbfaae7d9caa3d01ed78
Simon1703 <157050226+Simon1703@users.noreply.github.com>
2025-10-10 13:37:55 +0200

    Uebung1

--- exercise-000/main.cpp
+++ exercise-000/main.cpp
@@ -3,6 +3,18 @@
 
 #include "config.h"
 
+// Teil 3
+void changeValueByParameter(int value){
+    value= 99;
+    fmt::println("Variable wurde innerhalb der Funktion geändert auf: {}", value);
+}
+
+void changeValueByPointer(int* ptr){
+    *ptr = 77;
+    fmt::println("Variable wurde innerhalb der Funktion geändert auf: {}", *ptr);
+}
+
+
 int main(int argc, char **argv) {
 
     /**
@@ -12,6 +24,60 @@ int main(int argc, char **argv) {
      */
     fmt::print("Hello, {}!\n", tea::PROJECT_NAME);
 
+    // Teil 0
+    for (int i= 0; i<argc; i++) {
+        fmt::println("argc: {}, argument: {} (Adresse: {})", i, argv[i], fmt::ptr(argv[i]));
+    }
+
+    // Teil 1
+    // 1.1
+    int x= 10;
+
+    // 1.2
+    int* p= &x;
+
+    // 1.3
+    fmt::println("Wert von x: {}", x);
+    fmt::println("Adresse von x: {}", fmt::ptr(&x));
+
+    // 1.4
+    fmt::println("Wert von p: {}", fmt::ptr(p));
+    fmt::println("Wert, auf den p zeigt: {}", *p);
+    fmt::println("Adresse von p: {}", fmt::ptr(&p));
+
+    //. 1.5
+    *p= 42;
+
+    // 1.6
+    fmt::println("Neuer Wert von x: {}", x);
+    fmt::println("Adresse von p: {}", fmt::ptr(&p));
+    fmt::println("Neuer Wert, auf den p zeigt: {}", *p);
+
+
+    // Teil 2
+ 
+    double* po = new double(3.1415);   // Speicher im Heap reservieren
+    fmt::println("Wert: {}", *po);    // Zugriff über Dereferenzierung  
+    fmt::println("Adresse: {}", fmt::ptr(po));
+
+    delete po;                         // Speicher wieder freigeben
+
+
+    // Teil 3
+
+
+    int y= 5;
+    fmt::println("y ist nun: {}", y);
+
+    changeValueByParameter(y);
+    fmt::println("y ist außerhalb der Funktion nun: {}", y);
+    changeValueByPointer(&y);
+    fmt::println("y ist außerhalb der Funktion nun: {}", y);
+
+
+
+
+    
     /* INSERT YOUR CODE HERE */
 
     return 0; /* exit gracefully*/
